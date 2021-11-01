6610ab5deadf763167630644bc8564d38e99e2b3
Jonhyfun <jonhyfun@gmail.com>
2021-11-01 01:19:14 -0300

    Corrigindo typescript

---
+++ next.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    webpack: (config) => {
+      return Object.assign({}, config, {
+        externals: Object.assign({}, config.externals, {
+          fs: 'fs',
+        }),
+        module: Object.assign({}, config.module, {
+            rules: [
+                {
+                  test: /\.txt$/i,
+                  use: 'raw-loader',
+                },
+              ],
+            },
+        ),
+      });
+    }
+  };
