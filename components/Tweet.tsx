498473c4e05e63bba1ae6eff45030717c34cd053
Jonhyfun <jonhyfun@gmail.com>
2021-11-01 02:04:40 -0300

    responsividade

--- components/Tweet.tsx
+++ components/Tweet.tsx
@@ -1,5 +1,6 @@
 import Script from "next/script";
 import { useEffect, useState } from "react";
+import { isMobile, isTablet } from 'react-device-detect';
 
 export function Tweet({fakeText} : {fakeText: string}) {
     const [loading, setLoading] = useState(true);
@@ -16,13 +17,13 @@ export function Tweet({fakeText} : {fakeText: string}) {
     return(
         <div className="relative">
         {!loading && 
-            <div className="text-xl h-14 leading-6 absolute text-black bg-white" style={{top:'83.333px', left: '18px', width: '512px'}}>
+            <div className="mobileFakeText text-sm md:text-xl h-14 md:leading-6 absolute text-black bg-white" style={{top:'83.333px', left: '18px', width: '512px'}}>
                 {fakeText}
             </div>
         }
         <blockquote className="twitter-tweet text-transparent">
             <p lang="pt" dir="ltr">
-            Flow é maior que esse cancelamento, nós não vamos a lugar algum, nós não estamos sozinhos.
+                {(isMobile && !isTablet) ? 'Flow é maior que esse cancelamento,' : 'Flow é maior que esse cancelamento, nós não vamos a lugar algum, nós não estamos sozinhos.'}
             </p>
             &mdash; ♔ Monark (@monark) 
             <a href="https://twitter.com/monark/status/1454912673083232263?ref_src=twsrc%5Etfw">October 31, 2021</a>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import { AppProps } from 'next/app'
 
 import 'tailwindcss/tailwind.css'
@@ -7,6 +8,9 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </Head>
       <link rel="preconnect" href="https://fonts.googleapis.com"/>
       <link rel="preconnect" href="https://fonts.gstatic.com"/>
       <link href="https://fonts.googleapis.com/css2?family=Barlow:wght@400;500&display=swap" rel="stylesheet"/>
